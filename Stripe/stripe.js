3b4c21ce00852c1eee625acab291ad6ee3da9675
geoffrey-gallot <geoffrey.gallot@gmail.com>
2021-09-07 11:14:40 +0200

    commit snippet stripe

---
+++ Stripe/stripe.js
@@ -0,0 +1,57 @@
+
+//cdn  stripe
+//<script defer src="https://js.stripe.com/v3/"></script>
+
+//* clef public de l api
+let stripe = Stripe(
+  "public Key"
+);
+
+//* Cette méthode crée une instance de Elements, qui gère un groupe d'éléments.
+let elements = stripe.elements();
+
+//* creation du formulaire de carte bancaire
+let card = elements.create("card", { style: style });
+card.mount("#card-element");
+
+//* gestion de la saisie
+card.addEventListener("change", (e) => {
+  let displayError = document.getElementById("card-errors");
+  if (e.error) {
+    displayError.textContent = e.error.message;
+  } else {
+    displayError.textContent = "";
+  }
+});
+
+let form = document.getElementById("payment-form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let price = window.location["search"];
+  price = price.split("=")[1];
+  console.log(price);
+  stripe.createToken(card).then(function (result) {
+    if (result.error) {
+      //* gestion et affichage des erreurs
+      let errorElement = document.getElementById("card-errors");
+      errorElement.textContent = result.error.message;
+    } else {
+      //* creation du token
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+//*gestion du token et envoi en back-end
+function stripeTokenHandler(token, price) {
+  let form = document.getElementById("payment-form");
+  let hiddenInput = document.createElement("input");
+  hiddenInput.setAttribute("type", "text");
+  hiddenInput.setAttribute("value", token.id);
+  hiddenInput.setAttribute("name", "tok_visa");
+  console.log(hiddenInput.value);
+  form.appendChild(hiddenInput);
+  form.submit();
+}
+
---
+++ Stripe/stripe.php
@@ -0,0 +1,38 @@
+<?php
+// use Dotenv\Dotenv;
+// use Stripe\Stripe;
+// use Stripe\StripeClient;
+
+//* securisation de la clef secret avec la lib phpdotenv
+//* composer require vlucas/phpdotenv
+//* Lib stripe
+//* composer require stripe/stripe-php
+    //recupération de la cles secrete
+$dotenv = Dotenv::createImmutable(__DIR__, '.env');
+$dotenv->load();
+$stripeKey = $_ENV['SK_TEST_KEY'];
+    //
+$stripe = new StripeClient(
+    $stripeKey
+);
+
+//TODO paiement mensualisé
+$stripe->subscriptions->create([
+    'customer' => $id,// id du client
+    'items' => [
+        ['price' => $price[0]],
+    ],
+]);;
+
+
+
+//? paiement unique
+$stripe->charges->create([
+    'amount' => $_COOKIE['prix'],
+    'currency' => 'eur',
+    'source' => 'tok_visa',
+    'description' => 'paiement de test',
+]);
+return $response->withRedirect('/');
+
+
